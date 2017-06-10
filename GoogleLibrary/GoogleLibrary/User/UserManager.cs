58b79af34c74d5c51f509b9b8dc887199bbf1eb5
Daniel Henk <henk.daniel98@gmail.com>
2017-06-10 12:09:45 +0200

    UserManager done

--- GoogleLibrary/GoogleLibrary/User/UserManager.cs
+++ GoogleLibrary/GoogleLibrary/User/UserManager.cs
@@ -1,7 +1,13 @@
 ﻿using Google.Apis.Auth.OAuth2;
 using Google.Apis.Auth.OAuth2.Responses;
+using Google.Apis.Util.Store;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace GoogleLibrary.User
 {
@@ -18,12 +24,12 @@ namespace GoogleLibrary.User
         public List<string> Scope { get; set; }
 
         /// <summary>
-        /// authenticate when refresh token expires
+        /// authenticate when auth token expires
         /// </summary>
         public bool StayAuthenticated { get; set; }
 
         /// <summary>
-        /// retry time in seconds if authentication fails 
+        /// time to wait in seconds after next attempt to get auth token
         /// </summary>
         public int RetryIn { get; set; }
 
@@ -40,23 +46,172 @@ namespace GoogleLibrary.User
         /// <summary>
         /// folder name(not the path) where auth token will be stored
         /// </summary>
-        public string DataStoreFolder { get; set; }
+        public string DataStoreFolder
+        {
+            get { return _dataStoreFolder; }
+            set
+            {
+                _fileDataStore = new FileDataStore(value);
+                _dataStoreFolder = value;
+            }
+        }
+
+        private string _dataStoreFolder;
 
         /// <summary>
         /// UserCredentials Token
         /// with which you can authenticate with other google apis
         /// </summary>
-
         public TokenResponse UserToken => _userCredential?.Token;
-
+        public static UserManager Instance;
 
         private CancellationToken _cancellationToken;
         private System.Timers.Timer _refreshTimer;
         private UserCredential _userCredential;
+        private FileDataStore _fileDataStore;
 
-        public UserManager()
+        #region Timer
+        private void StartRefreshTimer(TimeSpan time)
         {
+            _refreshTimer = new System.Timers.Timer(time.TotalMilliseconds);
+            _refreshTimer.Elapsed += this._refreshTimer_Elapsed;
+            _refreshTimer.Start();
+        }
 
+        private async void _refreshTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            _refreshTimer.Dispose();
+            await GetOrRefreshCredential();
+        }
+        #endregion
+
+        /// <summary>
+        /// get the client secrets from a json file
+        /// </summary>
+        /// <param name="Path">json file path</param>
+        /// <returns>client secrets</returns>
+        public static ClientSecrets GetClientSecretsFromFile(string Path)
+        {
+            using (var stream = new FileStream(Path, FileMode.Open, FileAccess.Read))
+                return GoogleClientSecrets.Load(stream).Secrets;
+        }
+
+        /// <summary>
+        /// new instance of <cref="UserManager">
+        /// </summary>
+        /// <param name="ClientSecrets">client secrets</param>
+        /// <param name="User">application name</param>
+        /// <param name="Endpoint">endpoint provided by the api</param>
+        /// <param name="Scope">scope</param>
+        /// <param name="DataStoreFolder">foldername where auth token will be stored</param>
+        /// <param name="StayAuthenticated">authenticate when auth token expires</param>
+        /// <param name="RetryIn">time to wait in seconds after next attempt to get auth token</param>
+        public async static Task<UserManager> Initialize(ClientSecrets ClientSecrets, string User, string Endpoint, List<string> Scope, string DataStoreFolder, bool StayAuthenticated = true, int RetryIn = 30)
+        {
+            // TODO: better solution to check for invalid input?
+            ClientSecrets.ClientId.CheckNull(nameof(ClientSecrets.ClientId));
+            ClientSecrets.ClientSecret.CheckNull(nameof(ClientSecrets.ClientSecret));
+            Endpoint.CheckNull(nameof(Endpoint));
+            Scope.CheckNull(nameof(Scope));
+            User.CheckNull(nameof(User));
+            DataStoreFolder.CheckNull(nameof(DataStoreFolder));
+
+            Instance = new UserManager(ClientSecrets, User, Endpoint, Scope, DataStoreFolder, StayAuthenticated, RetryIn);
+            await Instance.GetOrRefreshCredential();
+            return Instance;
+        }
+
+
+        /// <summary>
+        /// new instance of <cref="UserManager">
+        /// </summary>
+        /// <param name="ClientSecrets">client secrets</param>
+        /// <param name="User">application name</param>
+        /// <param name="Endpoint">endpoint provided by the api</param>
+        /// <param name="Scope">scope</param>
+        /// <param name="DataStoreFolder">foldername where auth token will be stored</param>
+        /// <param name="StayAuthenticated">authenticate when auth token expires</param>
+        /// <param name="RetryIn">time to wait in seconds after next attempt to get auth token</param>
+        private UserManager(ClientSecrets ClientSecrets, string User, string Endpoint, List<string> Scope, string DataStoreFolder, bool StayAuthenticated = true, int RetryIn = 30)
+        {
+            this.ClientSecrets = ClientSecrets;
+            this.User = User;
+            this.Endpoint = Endpoint;
+            this.Scope = Scope;
+            this.DataStoreFolder = DataStoreFolder;
+            this.StayAuthenticated = StayAuthenticated;
+            this.RetryIn = RetryIn;
+        }
+
+        private async Task GetOrRefreshCredential()
+        {
+            if (_userCredential == null)
+                _userCredential = await GoogleWebAuthorizationBroker.AuthorizeAsync(ClientSecrets, Scope, User, _cancellationToken, _fileDataStore);
+
+            await _userCredential.RefreshTokenAsync(_cancellationToken);
+            StartRefreshTimer(TimeSpan.FromSeconds(_userCredential.Token.ExpiresInSeconds ?? RetryIn));
+        }
+
+        /// <summary>
+        /// get the avaible google user data
+        /// </summary>
+        /// <param name="AccessToken">the access token in the auth token</param>
+        /// <param name="SaveOnDisc">save it in the application folder as a json file</param>
+        /// <returns>user data</returns>
+        public async Task<GoogleUserData> GetUserData(string AccessToken, bool SaveOnDisc = false)
+        {
+            GoogleUserData userData = null;
+
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    // TODO: do i need it?
+                    client.CancelPendingRequests();
+
+                    var profileUrl = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=" + AccessToken;
+                    var output = await client.GetAsync(profileUrl);
+
+                    if (output.IsSuccessStatusCode)
+                    {
+                        var result = await output.Content.ReadAsStringAsync();
+                        userData = JsonConvert.DeserializeObject<GoogleUserData>(result);
+
+                        if (SaveOnDisc && userData != null)
+                            File.WriteAllText(Path.Combine(_fileDataStore.FolderPath, "GoogleUserData.json"), result);
+                    }
+                    else
+                    {
+                        userData = new GoogleUserData()
+                        {
+                            id = "-1",
+                            family_name = $"status code: {output.StatusCode.ToString()}",
+                            name = output.Content?.ReadAsStringAsync().Result,
+                        };
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                userData = new GoogleUserData()
+                {
+                    id = "-1",
+                    name = ex.ToString()
+                };
+            }
+
+            return userData;
+        }
+
+        /// <summary>
+        /// logout and revoke authtoken
+        /// </summary>
+        public void Logout()
+        {
+            _cancellationToken = new CancellationToken(true);
+            _refreshTimer.Dispose();
+            _userCredential?.RevokeTokenAsync(CancellationToken.None).Wait();
+            _userCredential = null;
         }
     }
 }
---
+++ GoogleLibrary/GoogleLibrary/Util.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GoogleLibrary
+{
+    public static class Util
+    {
+        public static void CheckNull(this object obj, string caller = "")
+        {
+            if (obj == null)
+                throw new ArgumentException("value cannot be null", caller);
+
+            if (obj is string s)
+                if (string.IsNullOrEmpty(s))
+                    throw new ArgumentException("value cannot be empty", caller);
+
+            // TODO: generic type?
+            if (obj is ICollection<string> l)
+                if (l.Count < 1)
+                    throw new ArgumentException("value cannot be empty", caller);
+        }
+    }
+}
